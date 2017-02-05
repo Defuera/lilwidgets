347a33959136a481daba6c964fc93140f7e3ce4a
defuera <vdenis@zvooq.com>
2017-02-05 15:04:52 +0300

    add progress dialog

--- app/src/main/java/ru/justd/lilwidgets/MainActivity.kt
+++ app/src/main/java/ru/justd/lilwidgets/MainActivity.kt
@@ -2,11 +2,17 @@ package ru.justd.lilwidgets
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ru.justd.library.ProgressDialogFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById(R.id.progress_dialog_button).setOnClickListener {
+            ProgressDialogFragment.Builder(supportFragmentManager)
+                    .create()
+        }
     }
 }
