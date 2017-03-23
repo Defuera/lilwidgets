5a8af4c4962ab32aea0694815e59b5ebfffbe4d0
shc <vasily.shc@gmail.com>
2017-03-23 14:01:06 +0300

    update demo

--- app/src/main/java/ru/justd/demo/ListActivity.kt
+++ app/src/main/java/ru/justd/demo/ListActivity.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import ru.justd.lilwidgets.recycler.LilMoveCallback
 import ru.justd.lilwidgets.recycler.LilRecyclerView
 import ru.justd.lilwidgets.recycler.LilRecyclerView.DragTrigger.HANDLE
 
@@ -18,11 +20,19 @@ class ListActivity : Activity() {
         setContentView(R.layout.activity_list)
 
         val list = findViewById(R.id.list) as LilRecyclerView
-        list.adapter = TestAdapter()
+        val testAdapter = TestAdapter()
+
+        list.adapter = testAdapter
         list.layoutManager = LinearLayoutManager(this)
 
         list.dragTrigger = HANDLE
         list.handleViewId = R.id.handle
+
+        list.moveCallback = object : LilMoveCallback {
+            override fun onItemMoved(current: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder) {
+                testAdapter.swapItems(current.adapterPosition, target.adapterPosition)
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/ru/justd/demo/TestAdapter.kt
+++ app/src/main/java/ru/justd/demo/TestAdapter.kt
@@ -1,12 +1,11 @@
 package ru.justd.demo
 
-import android.graphics.Color
 import android.support.v7.widget.RecyclerView
-import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import java.util.*
 
 /**
  * Created by shc on 21/03/2017.
@@ -16,12 +15,7 @@ class TestAdapter : RecyclerView.Adapter<TestAdapter.TestViewHolder>() {
     val items = ArrayList<Int>()
 
     init {
-        items.add(0)
-        items.add(1)
-        items.add(2)
-        items.add(3)
-        items.add(4)
-        items.add(5)
+        items += 0..50
     }
 
     override fun getItemCount(): Int = items.count()
@@ -30,22 +24,12 @@ class TestAdapter : RecyclerView.Adapter<TestAdapter.TestViewHolder>() {
         holder?.bind(items[position])
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestViewHolder {
-        val view = TextView(parent.context)
-        view.layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        view.setPadding(10, 30, 10, 30)
-        view.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
-        view.setBackgroundColor(Color.LTGRAY)
-
-        return TestViewHolder(
-                LayoutInflater
-                        .from(parent.context)
-                        .inflate(R.layout.item_demo, parent, false)
-        )
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestViewHolder =
+            TestViewHolder(
+                    LayoutInflater
+                            .from(parent.context)
+                            .inflate(R.layout.item_demo, parent, false)
+            )
 
     inner class TestViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
@@ -53,6 +37,10 @@ class TestAdapter : RecyclerView.Adapter<TestAdapter.TestViewHolder>() {
             (view.findViewById(R.id.text) as TextView).text = value.toString()
         }
 
+    }
+
+    fun swapItems(from: Int, to: Int) {
+        Collections.swap(items, from, to)
     }
 
 }
