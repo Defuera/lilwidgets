b739bff50a34e66f3e8a9b9d1db7e33a80aace7f
defuera <vdenis@zvooq.com>
2017-05-23 14:45:26 +0200

    implement liloaderdialog simple test

---
+++ app/src/test/java/ru/justd/demo/LilLoaderDialogTest.kt
@@ -0,0 +1,74 @@
+package ru.justd.demo
+
+import android.os.Build
+import android.support.v4.app.FragmentManager
+import android.view.View
+import android.widget.TextView
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import ru.justd.lilwidgets.LilLoaderDialog
+
+/**
+ * Created by defuera on 23/05/2017.
+ */
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class, sdk = intArrayOf(Build.VERSION_CODES.M))
+class LilLoaderDialogTest {
+
+    val activity: MainActivity = Robolectric.buildActivity(MainActivity::class.java)
+            .create()
+            .start()
+            .resume()
+            .get()
+
+    val fragmentManager: FragmentManager = activity.supportFragmentManager
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun simpleTest() {
+
+        //setup
+        LilLoaderDialog
+                .Builder(fragmentManager)
+                .setTitle("title")
+                .setMessage("message")
+                .create()
+
+        val dialog = fragmentManager.fragments[0]
+
+
+        //actual test
+        assertNotNull(dialog)
+
+        val view = dialog.view
+
+        view
+                ?.let {
+                    assertTextView(view, R.id.title, "title")
+                    assertTextView(view, R.id.message, "message")
+                }
+                ?: throw IllegalArgumentException("view must not be null")
+
+
+        assertNotNull(view.findViewById(R.id.progress_bar))
+    }
+
+    private fun assertTextView(view: View, resId: Int, text: String) {
+        val title = view.findViewById(resId) as TextView
+        assertNotNull(title)
+        assertEquals(text, title.text)
+    }
+
+}
