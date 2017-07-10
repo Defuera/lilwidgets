226489fb8487863152b5dbf363a82fcfb7e99d3e
shc <vasily.shc@gmail.com>
2017-07-10 18:02:13 +0300

    draw vertical seek bar

---
+++ app/src/main/java/ru/justd/demo/SeekBarActivity.kt
@@ -0,0 +1,7 @@
+package ru.justd.demo
+
+/**
+ * Created by shc on 06/07/2017.
+ */
+class SeekBarActivity {
+}
---
+++ library/src/main/java/ru/justd/lilwidgets/LilSeekBar.kt
@@ -0,0 +1,7 @@
+package ru.justd.lilwidgets
+
+/**
+ * Created by shc on 06/07/2017.
+ */
+class LilSeekBar {
+}
